4f6a1bc0999695cdb7bc81a8db090c4ad605c13e
XQYWorld <helloworldxqy@hotmail.com>
2026-04-30 05:08:53 +0000

    buildable

--- app/src/main/java/com/core/Main.java
+++ app/src/main/java/com/core/Main.java
@@ -2,7 +2,7 @@ package com.example.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MyPlugin extends JavaPlugin {
+public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("插件已启用！");
