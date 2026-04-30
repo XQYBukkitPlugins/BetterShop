5da0d62ce29c02da68346928c7ae76400b703b12
XQYWorld <helloworldxqy@hotmail.com>
2026-04-30 03:32:40 +0000

    inital commit

---
+++ app/src/main/java/com/core/Main.java
@@ -0,0 +1,15 @@
+package com.example.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("插件已启用！");
+    }
+    
+    @Override
+    public void onDisable() {
+        getLogger().info("插件已禁用！");
+    }
+}
